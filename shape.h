81cf0ed509bb1aced40ffa91a786fb19d7fb347e
Ashley Coleman <me@ashleycoleman.me>
2018-11-04 17:19:46 -0600

    Week 11 Starting Files

---
+++ shape.h
@@ -0,0 +1,24 @@
+//
+//  shape.h
+//  Week 11 
+//
+//  Created by Ashley Coleman on 7/5/18.
+//  Copyright © 2018 Ashley Coleman. All rights reserved.
+//
+
+#ifndef shape_h
+#define shape_h
+
+typedef struct Shape Shape;
+
+// 1) typedef a function pointer called "perimeter_function" that takes a "Shape *" 
+//        and returns a "float"
+
+// 2) typedef a function pointer called "area_function" that takes a "Shape *"
+//        and returns a "float"
+
+// 3) Define a struct (no typedef) called "Shape"
+//        that has a varible of type "perimeter_function" called "perimeter"
+//        and a variable of type "area_function" called "area"
+
+#endif /* shape_h */
